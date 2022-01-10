70d3c611d18802bb9e64e83bc735d0ce8cb0b9e2
snirappi <shawn.nirappil@gmail.com>
2022-01-10 12:11:37 -0600

    default emojis, defined check-in day, weekly/daily option, date formatting, general cleanup

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Discord Bot for Tracking New Years Resolutions
 
 # Features
 * Set, View, and Give updates on a New Year's Resolution
-* Every Sunday, shows everyone participating that has kept up to date with their resolution in the past week
+* Users can either choose weekly participation or daily participation
+* Shows everyone participating that has kept up to date with their resolution, for daily and weekly goals
 
 ## Details
 Creates a json file to store resolutions in case the bot is stopped for whatever reason.
@@ -11,3 +12,6 @@ Uses a .env file looking for
 * DISCORD_TOKEN 
 * JSON_FILE (name of file to use for storage)
 * CHANNEL (channel id to post to)
+* SUCCESS_EMOJI (user choice emoji)
+* FAIL_EMOJI (user choice emoji)
+* WEEKLY_CHECK_DAY (weekday day, number from 0-6 | Monday-Sunday )
--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from json.decoder import JSONDecodeError
 import os
 import json
-from datetime import date, timedelta
+from sys import platform
+from datetime import date, datetime, timedelta
 from discord.ext.commands.errors import MissingRequiredArgument
 from dotenv import load_dotenv
 from discord.ext import commands, tasks
@@ -13,6 +14,17 @@ CHANNEL = os.getenv('CHANNEL')
 JSON_FILE = os.getenv('JSON_FILE')
 SUCCESS_EMOJI = os.getenv('SUCCESS_EMOJI')
 FAIL_EMOJI = os.getenv('FAIL_EMOJI')
+
+if SUCCESS_EMOJI is None:
+    SUCCESS_EMOJI = '\u2705'
+if FAIL_EMOJI is None:
+    FAIL_EMOJI = '\u26D4'
+WEEKLY_CHECK_DAY = os.getenv('WEEKLY_CHECK_DAY')
+try:
+    WEEKLY_CHECK_DAY = int(WEEKLY_CHECK_DAY)
+except TypeError:
+    WEEKLY_CHECK_DAY = 6
+
 log = {}
 try: 
     with open(JSON_FILE, 'r') as openfile:
@@ -27,24 +39,28 @@ bot = commands.Bot(command_prefix='!')
 ##Commands
 @bot.command(name='all', help='Shows everyone\'s resolution')
 async def all(ctx):
+    if ctx.channel.id != int(CHANNEL):
+        return
     try:
         for user in log:
-            await ctx.send(f'{log[user]["name"]}: {log[user]["resolution"]} | Last Update: {log[user]["update_date"]}')
+            await ctx.send(f'{log[user]["name"]}: {log[user]["resolution"]} | {"Daily" if log[user]["daily"] else "Weekly"} | Last Update: {date_format(datetime.strptime(log[user]["update_date"], "%Y-%m-%d").date())}')
     except:
         pass
 
 @bot.command(name='res', help='Shows your resolution and status')
 async def resolution(ctx):
+    if ctx.channel.id != int(CHANNEL):
+        return
     try: 
-        print(log[str(ctx.author.id)])
         user_info = log[str(ctx.author.id)]
-        await ctx.send(f'{user_info["mention"]}\'s resolution: {user_info["resolution"]}\n \
-        Most Recent Update: {user_info["update"]} on {user_info["update_date"]}')
+        await ctx.send(f'{user_info["mention"]}\'s resolution: {user_info["resolution"]} | {"Daily" if user_info["daily"] else "Weekly"}\nMost Recent Update: \"{user_info["update"]}\" on {date_format(datetime.strptime(user_info["update_date"], "%Y-%m-%d").date())}')
     except KeyError: 
         await ctx.send('You don\'t have a resolution! use !set to make one')
 
 @bot.command(name='set', help='Sets your resolution for 2022. ex: !set "Eat Healthy"')
 async def set_resolution(ctx, resolution: str):
+    if ctx.channel.id != int(CHANNEL):
+        return
     id = str(ctx.author.id)
     log[id] = {}
     log[id]['resolution'] = resolution
@@ -52,39 +68,80 @@ async def set_resolution(ctx, resolution: str):
     log[id]['mention'] = '<@'+str(ctx.author.id)+'>'
     log[id]['update'] = ''
     log[id]['update_date'] = date.today().isoformat()
+    log[id]['daily'] = False
     response=resolution
-    print(log[ctx.author.id])
     dump_json()
     await ctx.send(f'{ctx.author.name}\'s New Year Resolution: ' + response)
 
 @bot.command(name='update', help='Tells everyone any updates on your resolution. ex: !updated "this is my update today"')
 async def update_resolution(ctx, update: str):
+    if ctx.channel.id != int(CHANNEL):
+        return
     try:
         log[str(ctx.author.id)]['update'] = update
         log[str(ctx.author.id)]['update_date'] = date.today().isoformat()
         dump_json()
         await ctx.send(f'Thanks for updating us {ctx.author.name}!')
     except MissingRequiredArgument:
-        await ctx.send(f'Don\'t forget to include the update ( !update "this is my update today")')
+        await ctx.send(f'Put an update ( !update "this is my update today")')
+    except KeyError:
+        await ctx.send('You don\'t have a resolution! use !set to make one')
+
+@bot.command(name='daily', help='Sets bot to check-in daily')
+async def set_daily_mode(ctx):
+    if ctx.channel.id != int(CHANNEL):
+        return
+    try:
+        log[str(ctx.author.id)]['daily'] = True
+        dump_json()
+        await ctx.send(f'{ctx.author.name} is checking in daily!')
+    except KeyError:
+        await ctx.send('You don\'t have a resolution! use !set to make one')
+
+@bot.command(name='weekly', help='Sets bot to check-in weekly')
+async def set_weekly_mode(ctx):
+    if ctx.channel.id != int(CHANNEL):
+        return
+    try:
+        log[str(ctx.author.id)]['daily'] = False
+        dump_json()
+        await ctx.send(f'{ctx.author.name} is checking in weekly!')
+    except KeyError:
+        await ctx.send('You don\'t have a resolution! use !set to make one')
 
 ##Tasks
 @tasks.loop(hours=24)
 async def announce_statuses():
-    if date.today().weekday() != 6:
-        return
+    DAILY_DELTA = date.today()-timedelta(days=1)
+    WEEKLY_DELTA = date.today()-timedelta(days=7)
     channel = await bot.fetch_channel(int(CHANNEL))
-    await channel.send(f'This week\'s resolution results: ')
+    if date.today().weekday() == WEEKLY_CHECK_DAY:
+        await channel.send(f'Weekly Resolution Results {date_format(WEEKLY_DELTA)} - {date_format(date.today())}: ')
+    else:
+        await channel.send(f'Daily Resolution Results {date_format(date.today())}: ')
     for user in log:
-        if date.fromisoformat(log[user]['update_date']) > (date.today()-timedelta(days=7)):
-            await channel.send(f'{log[user]["mention"]} {SUCCESS_EMOJI}')
-        else:
-            await channel.send(f'{log[user]["mention"]} {FAIL_EMOJI}')
+        if date.today().weekday() == WEEKLY_CHECK_DAY and log[user]['daily'] == False: 
+            if date.fromisoformat(log[user]['update_date']) > WEEKLY_DELTA:
+                await channel.send(f'{log[user]["mention"]} {SUCCESS_EMOJI}')
+            else:
+                await channel.send(f'{log[user]["mention"]} {FAIL_EMOJI}')
+        elif log[user]['daily'] == True: 
+            if date.fromisoformat(log[user]['update_date']) > DAILY_DELTA:
+                await channel.send(f'{log[user]["mention"]} {SUCCESS_EMOJI}')
+            else:
+                await channel.send(f'{log[user]["mention"]} {FAIL_EMOJI}')
 
 ##Helpers
 def dump_json():
     json_object = json.dumps(log, indent = 4)
     with open(JSON_FILE, 'w') as outfile:
         outfile.write(json_object)
+
+def date_format(date: date):
+    if platform == "win32" or platform == "cygwin": 
+        return date.strftime("%#m/%#d")
+    elif platform == "darwin" or platform == "linux" or platform == "linux2":
+        return date.strftime("%-m/%-d")
 
 announce_statuses.start()
 bot.run(TOKEN)
